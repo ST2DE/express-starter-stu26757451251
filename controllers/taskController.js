a0a6427e0b49aba7f913f6f7a7970f353748df72
CuRse <stu26757451251@gmail.com>
2018-05-04 16:46:55 +0800

    end ejs engine part

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,18 +1,28 @@
 var db = require('../models');
 var Task = db.Task;
 
+
 let taskController = {
-  index: function (req, res) {
-    Task.findAll()
-      .then(function (tasks) {
-        res.render('index', {"tasks": tasks});
-      });
-  },
-  indexApi: function (req, res) {
-    Task.findAll()
-      .then(function (tasks) {
-        res.json(tasks);
-      });
-  },
+    index: function (req, res) {
+        Task.findAll()
+            .then(function (tasks) {
+                res.render('index', { "tasks": tasks });
+            });
+    },
+    indexApi: function (req, res) {
+        Task.findAll()
+            .then(function (tasks) {
+                res.json(tasks);
+            });
+    },
+    todo: function (req, res) {
+        Task.findOrCreate({ where: { id: req.body.id }, defaults: { title: req.body.title } })
+        .spread((Task, created) => {
+            console.log(Task.get({
+                 plain:true
+            }))
+            console.log(created)
+        })
+    }
 };
 module.exports = taskController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const taskController = require('../controllers/taskController.js');
+const todoitem = require('../controllers/todoitem.js')
+
+
 
 module.exports = function (app) {
-
-  app.get('/tasks', taskController.index);
-  app.get('/api/tasks', taskController.indexApi);
-
+    app.get('/tasks', taskController.index);
+    app.get('/api/tasks', taskController.indexApi);
+    app.post('/tasks', taskController.todo);
 };
+
