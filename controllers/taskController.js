83bb5e76e29e7e6af1e9b022eb94e82b976f95f0
CuRse <stu26757451251@gmail.com>
2018-05-06 10:58:34 +0800

    Update todolist web

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,7 @@
 var db = require('../models');
 var Task = db.Task;
+var user = db.user;
+
 
 
 let taskController = {
@@ -15,7 +17,7 @@ let taskController = {
                 res.json(tasks);
             });
     },
-    todo: function (req, res) {
+    todo: function (req, res,next) {
         Task.findOrCreate({ where: { id: req.body.id }, defaults: { title: req.body.title } })
         .spread((Task, created) => {
             console.log(Task.get({
@@ -23,6 +25,18 @@ let taskController = {
             }))
             console.log(created)
             })
+            .then(function () {
+                return res.redirect('/tasks');
+            })
+    },
+    newmember: function (req, res,next) {
+        user.findOrCreate({ where: { username: req.body.username }, defaults: { password: req.body.password } })
+        .spread((user, created) => {
+            console.log(user.get({
+                plain: true
+            }))
+            console.log(created)
+        })
     }
 };
 module.exports = taskController;
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ module.exports = function (app) {
     app.get('/tasks', taskController.index);
     app.get('/api/tasks', taskController.indexApi);
     app.post('/tasks', taskController.todo);
-   
+    app.post('/adduser', taskController.newmember);
 };
 
