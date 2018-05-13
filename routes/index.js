3742daac3f84ac789b413555fe7e59acb839efac
CuRse <stu26757451251@gmail.com>
2018-05-14 00:20:00 +0800

    Apart Login and Task page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const taskController = require('../controllers/taskController.js');
-
+const express = require('express');
+const app = express();
 
 
 module.exports = function (app) {
+    
+
+
     app.get('/tasks', taskController.index);
     app.get('/api/tasks', taskController.indexApi);
     app.post('/tasks', taskController.todo);
@@ -12,6 +16,10 @@ module.exports = function (app) {
     app.get('/delete', taskController.taskdelete);
 
     app.post('/adduser', taskController.newmember);
-    
+
+    app.get('/', function (req, res) {
+        res.render('getstart');
+    })
+
 };
 
